b4edacbfd237af0602f9f3a99d1bd52d5e884a4d
Ulrik Boll Djurtoft <Ullebe1@gmail.com>
2017-11-14 16:06:25 +0100

    Fixed user install

--- install.sh
+++ install.sh
@@ -2,8 +2,16 @@
 if [ ! -d $HOME/.local/bin ]; then
   mkdir $HOME/.local/bin
 fi
+if [ ! -d $HOME/.local/systemd/user ]; then
+  mkdir -p $HOME/.local/systemd/user
+fi
 cp dagensdatalog $HOME/.local/bin/dagensdatalog
-cp dagensdatalog.* $HOME/.config/systemd/user
+cp dagensdatalog.timer $HOME/.config/systemd/user/dagensdatalog.timer
+
+original_path="\/usr\/local\/bin\/dagensdatalog"
+new_path="\/home\/$(whoami)\/.local\/bin\/dagensdatalog"
+
+sed "s/${original_path}/${new_path}/g" $HOME/.local/systemd/user/dagensdatalog.service > $HOME/.local/systemd/user/dagensdatalog.service
 
 systemctl --user enable dagensdatalog.timer
 
